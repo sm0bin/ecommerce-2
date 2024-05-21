323708576aced15385fc0187ec6c15768816eccb
sm0bin <shehjad0mobin@gmail.com>
2024-05-21 22:35:09 +0600

    server run 2

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,13 @@ const PORT = process.env.PORT;
 let server: Server;
 
 async function main() {
-  server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  server = app
+    .listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    })
+    .on("error", (error: Error) => {
+      console.error(`Error occurred: ${error.message}`);
+    });
 }
 
 main();
